f022d1339ebfe8981728659bc06c7901e13727ce
cathjime <xcatherine.jimenez@gmail.com>
2021-01-19 15:12:23 -0500

    debugged app.css issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 import React from "react";
 import Header from "./Components/Header";
 import MemeGenerator from "./Containers/MemeGenerator";
