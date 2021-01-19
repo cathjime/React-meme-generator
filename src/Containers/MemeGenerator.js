0f83a1a05478e19465ccf2cf78aeee6d1a5f2450
cathjime <xcatherine.jimenez@gmail.com>
2021-01-19 16:06:55 -0500

    working fetch from memes api

--- src/Containers/MemeGenerator.js
+++ src/Containers/MemeGenerator.js
@@ -2,10 +2,19 @@ import React from "react";
 
 class MemeGenerator extends React.Component {
   state = {
-    topText:"", 
-    bottomText:"", 
-    randomImg: "http://i.imgflip.com/1bij.jpg"
+    topText: "",
+    bottomText: "",
+    randomImg: "http://i.imgflip.com/1bij.jpg",
+    allMemeImgs: [],
   };
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        this.setState({ allMemeImgs: resp.data.memes });
+      });
+  }
 
   render() {
     return <h2>THIS IS THE MEME GENERATOR</h2>;
